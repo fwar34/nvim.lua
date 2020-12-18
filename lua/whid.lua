abcb3f18c8c1eef9de57ebe3d688fbb8dc8011c0
fwar34 <fwar34@126.com>
2020-12-18 19:48:31 +0800

    update

--- lua/whid.lua
+++ lua/whid.lua
@@ -1,1 +1,45 @@
+local api = vim.api
+local buf, win
 
+local function create_window()
+    buf = api.nvim_create_buf(false, true)
+    api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+
+    -- get dimensions
+    local width = api.nvim_get_option('columns')
+    local height = api.nvim_get_option('lines')
+
+    -- calculate our floating window size
+    local win_height = math.ceil(height * 0.8 - 4)
+    local win_width = math.ceil(width * 0.8)
+
+    -- and its starting position
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
+
+    -- set some options
+    local opts = {
+        style = 'minimal',
+        relative = 'editor',
+        width = win_width,
+        height = win_height,
+        row = row,
+        col = col,
+    }
+
+    -- and finally create it with buffer attached
+    api.nvim_open_win(buf, true, opts)
+
+    local border_opts = {
+        style = 'minimal',
+        relative = 'editor',
+        width = win_width + 2,
+        height = win_height + 2,
+        row = row - 1,
+        col = col - 1,
+    }
+
+    local border_buf = api.nvim_create_buf(false, true)
+    local border_lines = {'╔' .. string.rep('=', win_width) .. '╗'}
+    local middle_line = '║' .. string.rep
+end
--- plugin/coc.vim
+++ plugin/coc.vim
@@ -151,7 +151,7 @@ let g:coc_snippet_next = '<c-j>'
 " Use <C-k> for jump to previous placeholder, it's default of coc.nvim
 let g:coc_snippet_prev = '<c-k>'
 " Use <C-j> for both expand and jump (make expand higher priority.)
-imap <C-j> <Plug>(coc-snippets-expand-jump)
+" imap <C-j> <Plug>(coc-snippets-expand-jump)
 " Use <leader>x for convert visual selected code to snippet
 xmap <leader>xc  <Plug>(coc-convert-snippet)
 
