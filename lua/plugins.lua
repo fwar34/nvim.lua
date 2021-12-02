44eff54fe3a54b5c591d7208de884a1287101236
fwar34 <fwar34@126.com>
2021-12-02 13:01:14 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -734,9 +734,6 @@ return require('packer').startup(function()
                     }
                 }
             }
-            -- To get fzf loaded and working with telescope, you need to call
-            -- load_extension, somewhere after setup function:
-            require('telescope').load_extension('fzf')
         end,
 
         requires = {
@@ -746,7 +743,14 @@ return require('packer').startup(function()
                     require('telescope').load_extension('project')
                 end
             },
-            {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+            {
+                'nvim-telescope/telescope-fzf-native.nvim', run = 'make',
+                config = function ()
+                    -- To get fzf loaded and working with telescope, you need to call
+                    -- load_extension, somewhere after setup function:
+                    require('telescope').load_extension('fzf')
+                end
+            },
             {
                 -- need install sqlit
                 "nvim-telescope/telescope-frecency.nvim",
