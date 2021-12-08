6aa24eb10b15ed84cd9e6e5816788587dc2cafc0
fwar34 <fwar34@126.com>
2021-12-08 11:27:00 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -756,6 +756,22 @@ return require('packer').startup(function()
             -- You dont need to set any of these options. These are the default ones. Only
             -- the loading is important
             require('telescope').setup {
+                defaults = {
+                    -- Default configuration for telescope goes here:
+                    -- config_key = value,
+                    mappings = {
+                        i = {
+                            -- map actions.which_key to <C-h> (default: <C-/>)
+                            -- actions.which_key shows the mappings for your picker,
+                            -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                            ["<C-h>"] = "which_key",
+                            ["<C-g>"] = "close",
+                        },
+                        n = {
+                            ["<C-g>"] = "close",
+                        }
+                    }
+                },
                 vimgrep_arguments = {
                     "rg",
                     "--color=never",
