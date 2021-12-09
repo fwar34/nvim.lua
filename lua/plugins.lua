5d9e95ee749c16ce3a2c40b7101c19e9a6f716ba
fwar34 <fwar34@126.com>
2021-12-09 10:18:49 +0800

    add comments

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -476,6 +476,7 @@ return require('packer').startup(function()
 
     use {'junegunn/fzf', run = './install --all', lock = true}
     use {
+        -- 需要使用最新版的 bat 来预览，可以直接在 release 页面下载
         'junegunn/fzf.vim', event = 'VimEnter *',
         config = function()
             vim.g.fzf_preview_window = { 'up:50%', 'ctrl-/' }
