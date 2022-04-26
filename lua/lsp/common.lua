dfe16d4f6ca8a3e8ce82d56961b55cbd5dfb460d
fwar34 <fwar34@126.com>
2022-04-26 09:48:37 +0800

    update

--- lua/lsp/common.lua
+++ lua/lsp/common.lua
@@ -3,6 +3,7 @@ function M.custom_lsp_attach(client)
     -- See `:help nvim_buf_set_keymap()` for more information
     vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
     vim.api.nvim_buf_set_keymap(0, 'n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
+    vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', {noremap = true})
     -- ... and other keymappings for LSP
 
     -- Use LSP as the handler for omnifunc.
