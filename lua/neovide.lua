b8b62a7392f76953a0d31901238b2e17ddad2a4f
fwar34 <fwar34@126.com>
2022-07-07 07:13:39 +0800

    update

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -4,6 +4,9 @@ local function setup()
     -- vim.o.guifont = "Jetbrains Mono"
     -- https://github.com/laishulu/Sarasa-Mono-SC-Nerd
     vim.o.guifont = 'Sarasa Mono SC Nerd'
+    vim.g.neovide_remember_window_size = true
+    -- vim.g.neovide_transparency = 0.9
+    vim.g.neovide_cursor_vfx_mode = "ripple"
 end
 
 setup()
