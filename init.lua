8c5e6c909416b6e170ad97e6480f2f742300a477
fwar34 <fwar34@126.com>
2022-07-07 06:54:52 +0800

    add settings for neovide

--- init.lua
+++ init.lua
@@ -16,3 +16,11 @@ require("autocmd")
 -- require("terminal").setup()
 require("lsp").setup()
 -- require("lsp")
+
+if vim.g.neovide then
+    vim.g.neovide_cursor_trail_legnth = 0
+    vim.g.neovide_cursor_animation_length = 0
+    -- vim.o.guifont = "Jetbrains Mono"
+    -- https://github.com/laishulu/Sarasa-Mono-SC-Nerd
+    vim.o.guifont = 'Sarasa Mono SC Nerd'
+end
