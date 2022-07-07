e27d9fde8a768a92644c3790d19202abfc0d946a
fwar34 <fwar34@126.com>
2022-07-07 11:10:43 +0800

    use vim.env.MYHOSTNAME instead of global.hostname

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,3 +1,5 @@
+local env = vim.env
+
 local function setup()
     vim.g.neovide_cursor_trail_legnth = 0
     vim.g.neovide_cursor_animation_length = 0
@@ -11,11 +13,14 @@ local function setup()
     -- vim.g.neovide_cursor_vfx_mode = 'wireframe'
     -- vim.g.neovide_cursor_vfx_mode = "ripple"
     vim.g.neovide_no_idle = true
-    if require('global').hostname == 'archlinux' then -- pve-archlinux
+    -- local hostname = require('global').hostname
+    if env.MYHOSTNAME == 'archlinux' then -- pve-archlinux
         -- https://github.com/laishulu/Sarasa-Mono-SC-Nerd
         vim.opt.guifont = 'Sarasa Mono SC Nerd:h13'
         -- vim.o.guifont = "Jetbrains Mono"
         -- vim.o.guifont = 'Sarasa Mono SC Nerd'
+    elseif env.MYHOSTNAME == 'ubuntu-work' then
+        vim.opt.guifont = 'Sarasa Mono SC Nerd:h12'
     end
 end
 
