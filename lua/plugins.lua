bda10432a01541e69cae7e17c8e3cb66e6a7d2de
fwar34 <fwar34@126.com>
2022-07-08 15:37:29 +0800

    add gy command for Comment.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1106,6 +1106,49 @@ return require('packer').startup(function()
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
+            -- {{{ https://github.com/numToStr/Comment.nvim/issues/70, https://github.com/numToStr/Comment.nvim/pull/73
+            local A = vim.api
+            function _G.___gdc(vmode)
+                local C = require('Comment.config')
+                local U = require('Comment.utils')
+                local Op = require('Comment.opfunc')
+
+                local range = U.get_region(vmode)
+                local cfg = C:get()
+                local ctx = {
+                    cmode = U.cmode.comment, -- Always comment the line
+                    cmotion = U.cmotion.line, -- Line action `gy2j`
+                    ctype = U.ctype.line, -- Use line style comment
+                    range = range,
+                }
+                local lcs, rcs = U.parse_cstr(cfg, ctx)
+                local lines = U.get_lines(range)
+
+                -- Copying the block
+                local srow = ctx.range.erow
+                A.nvim_buf_set_lines(0, srow, srow, false, lines)
+
+                -- Doing the comment
+                Op.linewise({
+                    cfg = cfg,
+                    cmode = ctx.cmode,
+                    lines = lines,
+                    lcs = lcs,
+                    rcs = rcs,
+                    range = range,
+                })
+
+                -- Move the cursor
+                local erow = srow + 1
+                local line = U.get_lines({ srow = srow, erow = erow })
+                local _, col = U.grab_indent(line[1])
+                A.nvim_win_set_cursor(0, { erow, col })
+            end
+
+            local opt = { silent = true, noremap = true }
+            A.nvim_set_keymap('x', 'gy', '<ESC><CMD>lua ___gdc(vim.fn.visualmode())<CR>', opt)
+            A.nvim_set_keymap('n', 'gy', '<CMD>set operatorfunc=v:lua.___gdc<CR>g@', opt)
+            -- }}}
         end
     }
 
