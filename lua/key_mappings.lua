df92ccdcb95c4e467e47a44cbc0b23134a16a2e4
fwar34 <fwar34@126.com>
2022-07-09 19:23:56 +0800

    fix function nil error

--- lua/key_mappings.lua
+++ lua/key_mappings.lua
@@ -475,8 +475,13 @@ function key_mappings:start()
 
     -- lsp-config
     self.lsp_config = {
-        -- ['n|<LocalLeader>=='] = function() vim.lsp.buf.formatting() end,
-        ['n|<LocalLeader>=='] = function() vim.lsp.buf.format {async = true} end,
+        ['n|<LocalLeader>=='] = function()
+            if vim.lsp.buf.format then
+                vim.lsp.buf.format {async = true}
+            else
+                vim.lsp.buf.formatting()
+            end
+        end,
     }
 
     -- trouble.nvim
