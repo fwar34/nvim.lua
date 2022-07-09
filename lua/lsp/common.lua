1ff580b6b5c5666b299c05018c3a18dee01493e1
fwar34 <fwar34@126.com>
2022-07-09 21:39:21 +0800

    update for lsp

--- lua/lsp/common.lua
+++ lua/lsp/common.lua
@@ -1,16 +1,17 @@
 -- https://github.com/neovim/nvim-lspconfig#Suggested-configuration
 local M = {}
+
+local opts = { noremap = true }
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>ee', vim.diagnostic.open_float, opts)
+
 function M.custom_lsp_attach(client, bufnr)
     -- See `:help nvim_buf_set_keymap()` for more information
     vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true })
     vim.api.nvim_buf_set_keymap(0, 'n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
     vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true })
     -- ... and other keymappings for LSP
-
-    local opts = { noremap = true }
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<space>ee', vim.diagnostic.open_float, opts)
 
     local bufopts = { noremap = true, buffer = bufnr }
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
@@ -24,6 +25,7 @@ function M.custom_lsp_attach(client, bufnr)
 
     -- Use LSP as the handler for omnifunc.
     --    See `:help omnifunc` and `:help ins-completion` for more information.
+    -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- For plugins with an `on_attach` callback, call them here. For example:
