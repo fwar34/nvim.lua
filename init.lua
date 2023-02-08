ac7958c0739085f28dad3830e159c2d2290ab809
fwar34 <fwar34@126.com>
2023-02-08 15:26:09 +0800

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 local vim = vim
 local api = vim.api
 
+-- https://github.com/nvim-tree/nvim-tree.lua
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.cmd('set termguicolors')
 
 require("options")
