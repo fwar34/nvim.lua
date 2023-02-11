41129822953a1aa0594067eb10ecc08463e0d522
fwar34 <fwar34@126.com>
2023-02-11 11:18:14 +0800

    update

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,3 +1,6 @@
+-- If <C-\> is pressed, the next key is sent unless it is <C-N> or <C-O>.
+-- Use <C-\><C-N> to return to normal mode. |CTRL-\_CTRL-N|
+-- Use <C-\><C-O> to execute one normal mode command and then return to terminal mode.
 return {
     -- Terminal
     -- {
