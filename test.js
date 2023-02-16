4584da8ae9d8f0675085ca7ff05224b90510d353
fwar34 <fwar34@126.com>
2023-02-16 23:14:54 +0800

    update

--- test.js
+++ test.js
@@ -1,35 +1,35 @@
-// var myObject = {
-//     myWord: 'hello',
-//     myFunc: function() {
-//         return this.myWord.toUpperCase();
-//     }
-// };
-// 
-// console.log(myObject.myFunc());
-// 
-// function otherFunc(s) {
-//     return (this.myWord + s).toUpperCase();
-// }
-// 
-// console.log(otherFunc.call(myObject, 'feng'));
-// console.log(otherFunc.apply(myObject, ['feng22']));
-// 
-// var func3 = otherFunc.bind(myObject);
-// console.log(func3('feng33'));
+var myObject = {
+    myWord: 'hello',
+    myFunc: function() {
+        return this.myWord.toUpperCase();
+    }
+};
+
+console.log(myObject.myFunc());
+
+function otherFunc(s) {
+    return (this.myWord + s).toUpperCase();
+}
+
+console.log(otherFunc.call(myObject, 'feng'));
+console.log(otherFunc.apply(myObject, ['feng22']));
+
+var func3 = otherFunc.bind(myObject);
+console.log(func3('feng33'));
 
 // ------------------------------------------------------------
-new Promise(function (resolve, _) {
+new Promise(function(resolve, _) {
     console.log(1111);
     resolve(2222);
-}).then(function (value) {
+}).then(function(value) {
     console.log('then1');
     console.log(value);
     return 3333;
-}).then(function (value) {
+}).then(function(value) {
     console.log('then2');
     console.log(value);
     throw "An error";
-}).catch(function (err) {
+}).catch(function(err) {
     console.log(err);
 });
 
@@ -50,7 +50,7 @@ print('1', 1000).then(function() {
     // 定时器触发的时候执行（因为返回的 promise 中 resolve 是在定时器触发的时候执行的），
     // 而不加 return 的时候下个 then 就是直接执行（所以相当于 4 秒定时器设置后立即设置了 3 秒定时器，
     // 所以结果是 3 秒先触发，再过 1 秒后 4 秒定时器触发）
-    return print('4', 4000); 
+    return print('4', 4000);
 }).then(function() {
     print('3', 3000);
 })
