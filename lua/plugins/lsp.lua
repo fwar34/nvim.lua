22ca68b6e849b6a9a39813a4da0e890bf3a81c33
fwar34 <fwar34@126.com>
2023-03-23 18:47:57 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -179,18 +179,18 @@ return {
           {
             {
               name = 'buffer',
-              option = {
-                -- Correct:
-                -- keyword_pattern = [[\k\+]],
-                get_bufnrs = function()
-                  local buf = vim.api.nvim_get_current_buf()
-                  local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-                  if byte_size > 1024 * 1024 then -- 1 Megabyte max
-                    return {}
-                  end
-                  return { buf }
-                end
-              }
+              -- option = {
+              --   -- Correct:
+              --   -- keyword_pattern = [[\k\+]],
+              --   get_bufnrs = function()
+              --     local buf = vim.api.nvim_get_current_buf()
+              --     local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+              --     if byte_size > 1024 * 1024 then -- 1 Megabyte max
+              --       return {}
+              --     end
+              --     return { buf }
+              --   end
+              -- }
             },
             { name = 'tags' },
             -- { name = 'orgmode' },
